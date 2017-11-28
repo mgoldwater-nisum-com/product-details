acad52d44a1ad04e7407126ac9d42ee855700828
Nisum <nisum@Admins-MacBook-Pro-8.local>
2017-11-27 17:08:44 -0800

    Add localStorage setting to product state

--- app/models/productState.js
+++ app/models/productState.js
@@ -1,11 +1,13 @@
 import {Model} from 'backbone';
+import {LocalStorage} from 'backbone.localstorage';
 
 const ProductState = Model.extend({
   defaults: {
     size: 'Small',
     color: 'Black',
     quantity: 1
-  }
+  },
+  localStorage: new LocalStorage('ProductState'),
 });
 
 const productState = new ProductState();
