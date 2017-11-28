5623107d8688de92b15a877431c34679c36fff0c
Nisum <nisum@Admins-MacBook-Pro-8.local>
2017-11-28 09:20:50 -0800

    Render upon model change

--- app/views/productInfo.js
+++ app/views/productInfo.js
@@ -2,7 +2,10 @@ import {View} from 'backbone.marionette';
 import productInfoTemplate from '../templates/productInfoTemplate.hbs';
 
 const ProductInfoView = View.extend({
-  template: productInfoTemplate
+  template: productInfoTemplate,
+  modelEvents: {
+    'change': 'render'
+  }
 });
 
 export default ProductInfoView;
