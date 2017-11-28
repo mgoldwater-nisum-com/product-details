8e06d440040ceaa20c846b2e5be49cef54e96979
Nisum <nisum@Admins-MacBook-Pro-8.local>
2017-11-28 10:49:27 -0800

    Validate that item quantity must be at least 1

--- app/models/productState.js
+++ app/models/productState.js
@@ -5,11 +5,28 @@ const ProductState = Model.extend({
   defaults: {
     size: 'Small',
     color: 'Black',
-    quantity: 1
+    quantity: 1,
+    invalid: false
   },
   localStorage: new LocalStorage('ProductState'),
+  validate: function(attrs) {
+    console.log('the validate function has been initiated');
+    console.log('attrs is: ', attrs);
+    if (attrs.quantity < 1) {
+      return 'You can\'t buy less than 1 of an item';
+    }
+  }
 });
 
 const productState = new ProductState();
+productState.on("invalid", function(model){
+  // console.log('invalid event is initiated!')
+  // console.log('model is: ', model);
+  model.set('invalid', true);
+});
+
+productState.on("sync", function(model){
+  model.set('invalid', false);
+});
 
 export default productState;
