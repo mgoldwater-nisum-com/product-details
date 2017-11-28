45cbe6e8a7422096468990c9fad9ebdabc8bcdb7
Nisum <nisum@Admins-MacBook-Pro-8.local>
2017-11-28 09:56:34 -0800

    Shipping Info is displayed upon shipping form submission

--- app/views/shippingInfo.js
+++ app/views/shippingInfo.js
@@ -2,7 +2,10 @@ import {View} from 'backbone.marionette';
 import shippingInfoTemplate from '../templates/shippingInfoTemplate.hbs';
 
 const ShippingInfoView = View.extend({
-  template: shippingInfoTemplate
+  template: shippingInfoTemplate,
+  modelEvents: {
+    'change': 'render'
+  }
 });
 
 export default ShippingInfoView;
