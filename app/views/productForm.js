85f3882835911b5e7677620509846c0d99d53ee6
Nisum <nisum@Admins-MacBook-Pro-8.local>
2017-11-27 17:11:05 -0800

    product State is set upon form submission

--- app/views/productForm.js
+++ app/views/productForm.js
@@ -2,7 +2,23 @@ import {View} from 'backbone.marionette';
 import productFormTemplate from '../templates/productFormTemplate.hbs';
 
 const ProductFormView = View.extend({
-  template: productFormTemplate
+  template: productFormTemplate,
+  events: {
+    'submit': 'setProductState'
+  },
+  setProductState: function(e){
+    e.preventDefault();
+    const arr = this.$el.serializeArray();
+    console.log(arr);
+    const data = arr.reduce( (dataToChange, currData) => {
+      console.log('currData is: ', currData);
+      dataToChange[currData.name] = currData.value;
+      return dataToChange;
+    }, {});
+    console.log(data);
+    this.model.save(data);
+    // console.log(this.model);
+  }
 });
 
 export default ProductFormView;
