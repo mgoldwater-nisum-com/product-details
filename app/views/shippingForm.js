c93dd295407946f933b0b8449f716d880f711c5f
Nisum <nisum@Admins-MacBook-Pro-8.local>
2017-11-28 09:54:16 -0800

    change shipping state upon submitting form

--- app/views/shippingForm.js
+++ app/views/shippingForm.js
@@ -2,7 +2,19 @@ import {View} from 'backbone.marionette';
 import shippingFormTemplate from '../templates/shippingFormTemplate.hbs';
 
 const ShippingFormView = View.extend({
-  template: shippingFormTemplate
+  template: shippingFormTemplate,
+  events: {
+    'submit': 'setShippingState'
+  },
+  setShippingState: function(e){
+    e.preventDefault();
+    const arrOfData = this.$el.serializeArray();
+    const formattedData = arrOfData.reduce( (dataToChange, currData) => {
+      dataToChange[currData.name] = currData.value;
+      return dataToChange;
+    }, {});
+    this.model.save(formattedData);
+  }
 });
 
 export default ShippingFormView;
