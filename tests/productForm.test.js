15ded59f18812f98808eedb0051f15e5cf7a8a87
Nisum <nisum@Admins-MacBook-Pro-8.local>
2017-11-29 11:23:32 -0800

    productForm tests the model

---
+++ tests/productForm.test.js
@@ -0,0 +1,15 @@
+import {ProductState} from '../app/models/productState';
+
+const testProductState = new ProductState();
+
+describe('productForm tests', function() {
+  it('does not validate when the quantity is less than 1', function() {
+    testProductState.set('quantity', -2);
+    expect(testProductState.validate(testProductState.attributes)).toBe('You can\'t buy less than 1 of an item');
+  });
+
+  it('does validate when the quantity is greater than or equal to 1 and shows the shipping address', function() {
+    testProductState.set('quantity', 2);
+    expect(testProductState.validate(testProductState.attributes)).toBe(undefined);
+  });
+});
