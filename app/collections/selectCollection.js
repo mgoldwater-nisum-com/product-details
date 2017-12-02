ee16054a99b27005877c3ed11ed3977a0383ab78
Nisum <nisum@Admins-MacBook-Pro-8.local>
2017-12-02 14:32:25 -0800

    rename stateSelectCollection to selectCollection since it will be used for other selects

--- app/collections/selectCollection.js
+++ app/collections/selectCollection.js
@@ -1,10 +1,10 @@
 import {Collection} from 'backbone';
 import stateData from '../helpers/determineSelectedState';
 
-const StateSelectCollection = Collection.extend({
+const SelectCollection = Collection.extend({
 
 });
 
-const stateSelectCollection = new StateSelectCollection(stateData);
+const stateSelectCollection = new SelectCollection(stateData);
 
 export default stateSelectCollection;
